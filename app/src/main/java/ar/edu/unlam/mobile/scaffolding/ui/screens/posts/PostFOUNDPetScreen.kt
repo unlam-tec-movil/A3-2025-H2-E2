07a23e444d21d64b98c802f63d29194265cfd291
cacamanRPG <cacamanmierdaman6@gmail.com>
2025-11-24 13:03:25 -0300

    Ktlint format hecho

--- app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/posts/PostFOUNDPetScreen.kt
+++ app/src/main/java/ar/edu/unlam/mobile/scaffolding/ui/screens/posts/PostFOUNDPetScreen.kt
@@ -86,7 +86,7 @@ fun PostFoundPet(
                         ) {
                             val pet =
                                 Pet(
-                                    name = "",
+                                    name = "PERDIDO",
                                     seenAt = seenAt,
                                     locality = locality,
                                     gender = selectedGender!!,
